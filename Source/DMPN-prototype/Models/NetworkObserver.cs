331f9fa5cd085b4dacfc8b58a6b4221a059c78f5
Tre Giles <trefighter2334@aol.com>
2012-03-15 03:52:57 -0400

    completed source organization

---
+++ Source/DMPN-prototype/Models/NetworkObserver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DMPN_prototype
+{
+    /// <summary>
+    /// This class holistically observers the phone's current
+    /// network situation, exposing events for other classes
+    /// to hook in to. Acts as a simple wrapper for a few of
+    /// WP7's network detection functions.
+    /// </summary>
+    public class NetworkObserver
+    {
+        // TODO: Flesh this class out when we leave the prototype stage
+
+        public delegate void PropertyChangeHandler(object sender, EventArgs e);
+        public event PropertyChangeHandler PropertyChange;
+
+        protected void OnPropertyChange(object sender, EventArgs e)
+        {
+            // Check if there are any Subscribers
+            if(PropertyChange != null)
+            {
+                // Call the Event
+                PropertyChange(this, e);
+            }
+        }  
+    }
+}
---
+++ Source/DMPN-prototype/Views/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using System.Diagnostics;
+
+namespace DMPN_prototype
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            FirstListBox.DataContext  = App.queueViewModel;
+            SecondListBox.DataContext = App.debugViewModel;
+
+            this.Loaded +=new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            App.queueViewModel.AddEntry(("65920"+((new Random(99999)).Next()).ToString())+"@tmobile", "spysrs2@gmail.com", "Lol.", QueueViewModel.DUMPED);
+            App.queueViewModel.AddEntry(("65920" + ((new Random(99999)).Next()).ToString()) + "@tmobile", "spysrs2@gmail.com", "test2.", QueueViewModel.PROPAGATING);
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtDestination.Text.Length == 0 || txtDestination.InDefaultState())
+                MessageBox.Show("You forgot to enter a Destination.");
+            else if(txtSource.Text.Length == 0 || txtSource.InDefaultState())
+                MessageBox.Show("You forgot to enter a Source.");
+            else if(txtMessage.Text.Length == 0 || txtMessage.InDefaultState())
+                MessageBox.Show("You forgot to enter a Messsage.");
+            else
+            {
+                App.queueViewModel.AddEntry(txtDestination.Text, txtSource.Text, txtMessage.Text);
+
+                txtDestination.SetDefaultText(true);
+                txtSource.SetDefaultText(true);
+                txtMessage.SetDefaultText(true);
+
+                MessageBox.Show("Your message has been added to your Queue!");
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult m = MessageBox.Show("This will clear sent and expired packets. Are you sure?", "Clear Packets?", MessageBoxButton.OKCancel);
+            if (m == MessageBoxResult.OK)
+            {
+                App.queueViewModel.cleanEntries();
+            }
+        }
+
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult m = MessageBox.Show("This will clear all packets. Are you sure?", "Clear Packets?", MessageBoxButton.OKCancel);
+            if (m == MessageBoxResult.OK)
+            {
+                App.queueViewModel.clearAllEntries();
+            }
+        }
+    }
+}
