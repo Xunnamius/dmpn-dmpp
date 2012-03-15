85165879d641662c7afacbb8ee0aa5a3bbb785e9
Tre Giles <trefighter2334@aol.com>
2012-03-15 14:20:48 -0400

    updates namespaces, added dummy files

---
+++ Source/DMPN-prototype/Models/Broadcaster.cs
@@ -0,0 +1,14 @@
+﻿namespace DMPN_prototype.Models.Broadcast
+{
+    // TODO: *Everything* needs to be documented!
+
+    /// <summary>
+    /// This class handles the adhoc point-to-point
+    /// (pseudo or otherwise) communications that
+    /// take place between DMPN-aware devices.
+    /// </summary>
+    public class Broadcaster
+    {
+        
+    }
+}
